ec9b879d4b1352f341656f9f0e8dea1cb21ee4f3
Mateusz Sawicki <mateusz.saw00@wp.pl>
2023-09-25 03:35:49 +0200

    h

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import PracticeComponent from './components/PracticeComponent'
+
+function App() {
+  return (
+    <PracticeComponent />
+  );
+}
+
+export default App;
---
+++ src/components/PracticeComponent.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import styles from './PracticeComponent.module.scss';
+
+function PracticeComponent() {
+
+  let x = '';
+
+  const text = 'abcdefghi';
+
+  for (let i = text.length - 1; i >= 0; i++ ) {
+    console.log(i);
+  }
+  
+  return (
+    <>
+      
+    
+    
+    
+    
+    
+    
+    </>
+  );
+}
+
+export default PracticeComponent;
